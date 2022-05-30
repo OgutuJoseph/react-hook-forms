e3f2f9ef1734b7823224a68dfce87d4876186654
OgutuJoseph <onyango.joseph@yahoo.com>
2022-05-30 11:59:50 +0300

    Data Register

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import './App.css';
 
 function App() {
+
+  const { register, handleSubmit } = useForm();
+  const [ userInfo, setUserInfo ] = useState();
+
+  const onSubmit = (data) => {
+    setUserInfo(data)
+    console.log('data: ', data)
+  }
+
   return (
     <div className="container">
-      <form>
+      <pre>{ JSON.stringify(userInfo, undefined, 2) }</pre>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <h1>Registration Form</h1>
         <div className='ui divider'></div>
         <div className='ui form'>
           <div className='field'>
             <label>Username</label>
-            <input type='text' name='username' placeholder='Username' />
+            <input type='text' name='username' placeholder='Username' {...register('username')} />
           </div>
           <div className='field'>
             <label>Email</label>
-            <input type='email' name='email' placeholder='Email' />
+            <input type='email' name='email' placeholder='Email' {...register('email')} />
           </div>
           <div className='field'>
             <label>Password</label>
-            <input type='password' name='password' placeholder='Password' />
+            <input type='password' name='password' placeholder='Password' {...register('password')} />
           </div>
+          <div>
+            <label></label>
+            <input type="file" multiple  {...register('file')} name='file' />
+          </div>
+          <button className='fluid ui button blue'>Submit</button>
         </div>
       </form>
     </div>
